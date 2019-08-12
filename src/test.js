979258bc4cdf5f78492d555d94812ac4d85533fe
ofroad <wh724724@163.com>
2019-08-12 15:07:56 +0800

    init

---
+++ src/test.js
@@ -0,0 +1,19 @@
+// eslint测试
+module.exports = {
+  plugins: {
+    autoprefixer: {}
+  }
+};
+const k = 12;
+const t = 'r';
+function test() {
+  const z = 1;
+  const k1 = 1;
+  console.log(z, k1);
+}
+const tt1 = { a: 1, b: 2, n: 66 };
+const kk1 = {
+  aa: 123,
+  bb: 12
+};
+console.log(k, t, test(), tt1, kk1);
